356072181d9c9a9159b85e7a3b3ae4152e8eaea8
Akeda Bagus <akeda@x-team.com>
2013-08-02 04:42:13 +0700

    Init.

---
+++ main.go
@@ -0,0 +1,14 @@
+// Copyright 2013 The gotog AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// gotog Command line client written in Go that speaks to Toggl API.
+package main
+
+import (
+	"github.com/gedex/go-toggl/toggl"
+	"github.com/gedex/gotog/clients"
+	"github.com/gedex/gotog/commands"
+	"github.com/gedex/gotog/utils"
+)
